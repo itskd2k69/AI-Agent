a1eb99a3c4c40db9fc736f82b5f145883191d09b
Kuldeep Amreliya <kuldeepamareliya69@gmail.com>
2025-12-26 23:43:33 +0530

    Clean start without secrets

---
+++ agent.py
@@ -0,0 +1,50 @@
+# Step 1 : Setup API Key for Groq , Open AI and Travily
+import os
+
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+
+# Step 2 : Setup LLM & Tools
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+from langchain_core.messages import SystemMessage, HumanMessage
+
+openai_llm = ChatOpenAI(model="gpt-4o-min")
+groq_llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+
+
+# Step 3: Setup AI Agents with Search tool functionality
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import AIMessage
+system_prompt="Act as an AI chatbot who is smart and friendly"
+
+def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider):
+
+    if provider=="Groq":
+        llm=ChatGroq(model=llm_id)
+    elif provider=="OpenAI":
+        llm=ChatOpenAI(model=llm_id)
+
+    llm = llm.bind(
+        messages=[SystemMessage(content=system_prompt)]
+    )
+    
+    tools = [TavilySearch(max_results=2)] if allow_search else []
+
+    agent = create_react_agent(
+        model=llm,
+        tools=tools
+    )
+
+    
+    state = {
+        "messages": [HumanMessage(content=query)]
+    }
+    response=agent.invoke(state)
+    messages=response.get("messages")
+    ai_messages=[message.content for message in messages if isinstance(message, AIMessage)]
+    return ai_messages[-1]
